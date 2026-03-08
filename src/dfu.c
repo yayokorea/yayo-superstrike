d9de7bd781a9b844c335a2bd4b03eb0d99057c5e
Yayo <jiwoowatch@gmail.com>
2026-03-08 09:21:37 +0900

    dfu 고치기 및 웹 개선

--- src/dfu.c
+++ src/dfu.c
@@ -1,10 +1,7 @@
 #include "dfu.h"
 
 #include <zephyr/sys/reboot.h>
-
-#ifdef NRF52_SERIES
 #include <hal/nrf_power.h>
-#endif
 
 #define DFU_MAGIC_UF2_RESET 0x57
 
@@ -15,10 +12,6 @@ void dfu_reboot(void)
 
 void dfu_reboot_to_bootloader(void)
 {
-#ifdef NRF52_SERIES
-    nrf_power_gpregret_set(NRF_POWER, 0, DFU_MAGIC_UF2_RESET);
+    NRF_POWER->GPREGRET = DFU_MAGIC_UF2_RESET;
     NVIC_SystemReset();
-#else
-    sys_reboot(DFU_MAGIC_UF2_RESET);
-#endif
 }

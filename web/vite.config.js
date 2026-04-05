cb24a1731ea6b8d3b7b7b70ad2427fb975c7fcbe
yayo <jiwoowatch@gmail.com>
2026-04-05 17:19:15 +0900

    웹 디자인 개편 빌드 산출물 추가

---
+++ web/vite.config.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+var __dirname = path.dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
+});

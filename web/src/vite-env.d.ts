0986f9ec822fad46ca55532859c48c0d81132f1c
yayo <jiwoowatch@gmail.com>
2026-04-08 02:25:07 +0900

    웹 OTA 채널 선택 UI 추가

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_GITHUB_REPO?: string;
   readonly VITE_OTA_BASE_URL?: string;
-  readonly VITE_RELEASE_CHANNEL?: string;
   readonly VITE_RELEASE_MANIFEST_URL?: string;
 }
 
